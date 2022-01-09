a3716e412675d7bf49394c6d47388297f1d6264f
Keerthivasan-Murugesan <keerthim226@gmail.com>
2022-01-09 20:40:20 +0530

    Files and Functions split - 1

--- TestFunctions.h
+++ TestFunctions.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <assert.h>
+#include "VariableDefinitions.h"
 
 void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
 void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
