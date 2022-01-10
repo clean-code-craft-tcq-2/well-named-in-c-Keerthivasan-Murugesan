7170fe4581945e46cd5f5ece8132b9fa666481d3
Keerthivasan-Murugesan <keerthim226@gmail.com>
2022-01-10 22:56:40 +0530

    Wiring manual update

--- TestFunctions.c
+++ TestFunctions.c
@@ -17,3 +17,22 @@ void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expected
     printf("Got pair number %d\n", pairNumber);
     assert(pairNumber == expectedPairNumber);
 }
+
+void PrintWiringManual(void){
+    unsigned int MajorColor_index = 0, MinorColor_index = 0;
+    unsigned int colorCode = 1;
+
+    printf("------- COLOUR CODING DATABASE -------\n");
+    for(MajorColor_index = 0; MajorColor_index < MaxPossibleColorsInMajorOrMinor; MajorColor_index++)
+    {
+        for(MinorColor_index = 0; MinorColor_index < MaxPossibleColorsInMajorOrMinor; MinorColor_index++)
+        {
+             printf("*********************");
+             printf("\n Major Color : %s  ", MajorColorNames[MajorColor_index]);
+             printf("\n Minor Color : %s  ", MinorColorNames[MinorColor_index]);
+             printf("\n Color Code  : %d  ", colorCode);
+             printf("*********************");
+             colorCode++;
+        }
+    }
+}
--- VariableFuncDeclarations.h
+++ VariableFuncDeclarations.h
@@ -17,9 +17,11 @@ typedef struct {
     enum MinorColor minorColor;
 } ColorPair;
 
+#define MaxPossibleColorsInMajorOrMinor (5u)
 void ColorPairToString(const ColorPair* colorPair, char* buffer);
 ColorPair GetColorFromPairNumber(int pairNumber);
 int GetPairNumberFromColor(const ColorPair* colorPair);
 
 void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
 void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
+void PrintWiringManual(void);
